206a36d1f5971e6d401f1c8a9949c95110323d36
Matheus Oliveira <matheushso99@hotmail.com>
2024-04-02 12:57:27 -0300

    first commit

---
+++ src/main/java/br/com/processor/order/OrderProcessorApplication.java
@@ -0,0 +1,20 @@
+package br.com.processor.order;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class OrderProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessorApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
---
+++ src/test/java/br/com/processor/order/OrderProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.processor.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
