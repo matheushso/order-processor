f08e757245a9600d4b3d7940bfd424a5ec6e6875
Matheus Oliveira <matheushso99@hotmail.com>
2024-04-03 10:42:20 -0300

    create order repository

---
+++ src/main/java/br/com/processor/order/repository/OrderRepository.java
@@ -0,0 +1,56 @@
+package br.com.processor.order.repository;
+
+import br.com.processor.order.entity.Order;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+
+    private final MongoTemplate mongoTemplate;
+
+    public OrderRepository(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public void saveOrder(Order order) {
+        mongoTemplate.save(order);
+    }
+
+    public List<Order> findOrdersWithOptionalFilters(String seller, LocalDate startDate, LocalDate endDate,
+                                                     String orderStatus, String paymentMethod, String paymentStatus) {
+        Query query = new Query();
+
+        query.addCriteria(Criteria.where("seller").is(seller));
+
+        if (startDate != null && endDate != null) {
+            long startTimestamp = startDate.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli();
+            long endTimestamp = endDate.atTime(LocalTime.MAX).toInstant(ZoneOffset.UTC).toEpochMilli();
+
+            query.addCriteria(Criteria.where("createdAt")
+                    .gte(startTimestamp)
+                    .lt(endTimestamp));
+        }
+
+        if (orderStatus != null) {
+            query.addCriteria(Criteria.where("status").is(orderStatus.toUpperCase()));
+        }
+
+        if (paymentMethod != null) {
+            query.addCriteria(Criteria.where("payment.method").is(paymentMethod.toUpperCase()));
+        }
+
+        if (paymentStatus != null) {
+            query.addCriteria(Criteria.where("payment.status").is(paymentStatus.toUpperCase()));
+        }
+
+        return mongoTemplate.find(query, Order.class);
+    }
+}
