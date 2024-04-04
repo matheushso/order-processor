7daf91975dbe563a3624310c81fdcf15d45c4e39
Matheus Oliveira <matheushso99@hotmail.com>
2024-04-03 21:53:50 -0300

    adjust API documentation

--- src/main/java/br/com/processor/order/controller/OrderController.java
+++ src/main/java/br/com/processor/order/controller/OrderController.java
@@ -1,11 +1,14 @@
 package br.com.processor.order.controller;
 
+import br.com.processor.order.dto.ErrorDTO;
 import br.com.processor.order.entity.Order;
 import br.com.processor.order.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +23,18 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping
-    @Operation(summary = "Permite buscar pedidos autenticando-se através JWT com sellerId no payload, filtrando por data inicial, data final, status do pedido, método de pagamento e status de pagamento.")
+    @Operation(summary = "Permite buscar pedidos autenticando-se através JWT com sellerId no payload, filtrando por data inicial, data final, status do pedido, método de pagamento e status de pagamento.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Busca de pedidos bem-sucedida",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(responseCode = "400", description = "Requisição inválida devido a um dos seguintes motivos: somente uma das datas (inicial ou final) foi informada," +
+                            " a data inicial é posterior à data final, ou um valor inválido foi fornecido para 'orderStatus', 'paymentMethod' ou 'paymentStatus'.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
+                    @ApiResponse(responseCode = "401", description = "Autenticação falhou. Isso pode ocorrer se o JWT não for fornecido ou se for inválido.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class))),
+                    @ApiResponse(responseCode = "500", description = "Erro interno da aplicação. Entre em contato com o suporte técnico para mais informações.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorDTO.class)))
+            })
     public List<Order> findOrdersWithOptionalFilters(@RequestHeader("Authorization") @Parameter(in = ParameterIn.HEADER, description = "JWT usado para autenticação. Deve conter o sellerId no payload.",
                                                              schema = @Schema(type = "string"),
                                                              example = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZWxsZXJJZCI6IjY2MDgxMDg0OGE3NTRlMGMzMmQ2Y2EyYSJ9.Me30gMGf-T15zr6FoPRLc6QnPT2KDn7JFA4Rg0Jte0c")
